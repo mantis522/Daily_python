a6b94161f27e13828a250e5f2bab294b4a1dd833
mantis522 <augustnorase@gmail.com>
2021-03-25 18:22:06 +0900

    RNN_Attention own data

--- deep_learning/RNN/Attention_RNN.py
+++ deep_learning/RNN/Attention_RNN.py
@@ -1,83 +1,92 @@
-import os
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from torchtext import data
 import random
-import pandas as pd
-import re
 from torchtext.data import TabularDataset
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm import tqdm
+import time
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 SEED = 1234
 
-torch.manual_seed(SEED)
-torch.backends.cudnn.deterministic = True
-
-TEXT = data.Field(sequential=True,
-                  use_vocab=True,
-                  tokenize=str.split,
-                  lower=True,
-                  batch_first=True)
-
-LABEL = data.LabelField(sequential=False,
-                   use_vocab=False,
-                   batch_first=False,
-                   is_target=True,
-                   dtype = torch.float)
+TEXT = data.Field(tokenize = str.split)
+LABEL = data.LabelField(dtype = torch.float)
 
 train_data, test_data = TabularDataset.splits(
-        path=r"D:\ruin\data\csv_file\imdb_split", train='train_data.csv', test='test_data.csv', format='csv',
-        fields=[('review', TEXT), ('sentiment', LABEL)], skip_header=True)
+            path=r"D:\ruin\data\csv_file\imdb_split", train='train_data.csv', test='test_data.csv', format='csv',
+            fields=[('review', TEXT), ('sentiment', LABEL)], skip_header=True)
 
-train_data, valid_data = train_data.split(random_state = random.seed(SEED))
+train_data, valid_data = train_data.split(random_state=random.seed(SEED))
+
+print('Number of train data {}'.format(len(train_data)))
+print('Number of val data {}'.format(len(valid_data)))
+print('Number of test data {}'.format(len(test_data)))
 
 MAX_VOCAB_SIZE = 25_000
 
 TEXT.build_vocab(train_data,
-                 max_size = MAX_VOCAB_SIZE,
-                 vectors = "glove.6B.100d",
-                 unk_init = torch.Tensor.normal_)
-
+                     max_size = MAX_VOCAB_SIZE,
+                     vectors = 'glove.6B.100d',
+                     unk_init = torch.Tensor.normal_)
 LABEL.build_vocab(train_data)
 
 BATCH_SIZE = 64
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
-    (train_data, valid_data, test_data),
-    batch_size = BATCH_SIZE,
-    sort_key=lambda x: len(x.review),
-    device = device)
+        (train_data, valid_data, test_data),
+        batch_size = BATCH_SIZE, sort_key=lambda x: len(x.review),
+        device = device)
 
 class RNN(nn.Module):
-    def __init__(self, n_layers, input_dim, embedding_dim, hidden_dim, output_dim, drop_out):
+    def __init__(self, input_dim, embedding_dim, hidden_dim, output_dim):
         super().__init__()
-        self.n_layers = n_layers
-        self.hidden_dim = hidden_dim
+
         self.embedding = nn.Embedding(input_dim, embedding_dim)
-        self.rnn = nn.RNN(embedding_dim, hidden_dim, n_layers, batch_first=True)
-        self.dropout = nn.Dropout(drop_out)
+        self.rnn = nn.GRU(embedding_dim, hidden_dim)
         self.fc = nn.Linear(hidden_dim, output_dim)
+        self.use_att = True
+        self.att = Attention(hidden_dim)
 
     def forward(self, text):
+        # text = [sent len, batch size]
         embedded = self.embedding(text)
-        h_0 = torch.zeros(self.n_layers, text.size(0), self.hidden_dim).to(device)
-        x, _ = self.rnn(embedded, h_0)
-        h_t = x[:,-1,:]
-        self.dropout(h_t)
 
-        return self.fc(h_t)
+        # embedded = [sent len, batch size, emb dim]
+        output, hidden = self.rnn(embedded)
+
+        # output = [sent len, batch size, hid dim]
+        # hidden = [1, batch size, hid dim]
+
+        h_t = hidden.view(text.shape[1], -1)
+        hidden = self.att(output, h_t)
+        pred = self.fc(hidden.squeeze(0)).view(-1, 1)
+
+        return pred
+
+class Attention(nn.Module):
+    def __init__(self, input_dim, hidden_dim=64):
+        super().__init__()
+        self.w1 = nn.Linear(input_dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(input_dim, hidden_dim)
+        self.V = nn.Linear(hidden_dim, 1)
+
+    def forward(self, input, z):
+        w1h = self.w1(input).transpose(0, 1)
+        w2h = self.w2(z).unsqueeze(1)
+        u_score = torch.tanh(w1h + w2h)
+        u_score = self.V(u_score)
+        att = torch.softmax(u_score, dim=1).transpose(1, 2)
+
+        return torch.bmm(att, input.transpose(0, 1)).unsqueeze(1)
 
 INPUT_DIM = len(TEXT.vocab)
 EMBEDDING_DIM = 100
 HIDDEN_DIM = 256
 OUTPUT_DIM = 1
-N_LAYER = 2
-DROPOUT = 0.5
 
-model = RNN(N_LAYER, INPUT_DIM, EMBEDDING_DIM, HIDDEN_DIM, OUTPUT_DIM, DROPOUT)
-print(model)
+model = RNN(INPUT_DIM, EMBEDDING_DIM, HIDDEN_DIM, OUTPUT_DIM)
 
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 criterion = nn.BCEWithLogitsLoss()
@@ -88,6 +97,7 @@ def binary_accuracy(preds, y):
     """
     Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
     """
+
     #round predictions to the closest integer
     rounded_preds = torch.round(torch.sigmoid(preds))
     correct = (rounded_preds == y).float() #convert into float for division
@@ -101,7 +111,7 @@ def train(model, iterator, optimizer, criterion):
 
     model.train()
 
-    for batch in iterator:
+    for batch in tqdm(iterator):
         optimizer.zero_grad()
 
         predictions = model(batch.review).squeeze(1)
@@ -139,17 +149,17 @@ def evaluate(model, iterator, criterion):
 
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
-import time
-
-N_EPOCHS = 5
-
-best_valid_loss = float('inf')
 
 def epoch_time(start_time, end_time):
     elapsed_time = end_time - start_time
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
     return elapsed_mins, elapsed_secs
+
+
+N_EPOCHS = 5
+
+best_valid_loss = float('inf')
 
 for epoch in range(N_EPOCHS):
 
@@ -164,7 +174,15 @@ for epoch in range(N_EPOCHS):
 
     if valid_loss < best_valid_loss:
         best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'tut1-model.pt')
 
     print(f'Epoch: {epoch + 1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
     print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
     print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc * 100:.2f}%')
+
+# testing
+model.load_state_dict(torch.load('tut1-model.pt'))
+
+test_loss, test_acc = evaluate(model, test_iterator, criterion)
+
+print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}%')
