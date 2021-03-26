d2a1fdd880ccba6251ad334927b570df7bae48e6
mantis522 <augustnorase@gmail.com>
2021-03-27 03:45:07 +0900

    Transformer simple practice

---
+++ deep_learning/Transformer/simple_prac.py
@@ -0,0 +1,68 @@
+import sentencepiece as spm
+import torch
+import torch.nn as nn
+import numpy as np
+
+# vocab loading
+vocab_file = r"D:\ruin\data\kowiki.model"
+vocab = spm.SentencePieceProcessor()
+vocab.load(vocab_file)
+
+# 입력 texts
+lines = [
+  "겨울은 추워요.",
+  "감기 조심하세요."
+]
+
+# text를 tensor로 변환
+inputs = []
+for line in lines:
+  pieces = vocab.encode_as_pieces(line)
+  ids = vocab.encode_as_ids(line)
+  inputs.append(torch.tensor(ids))
+  print(pieces)
+
+# 입력 길이가 다르므로 입력 최대 길이에 맟춰 padding(0)을 추가 해 줌
+inputs = torch.nn.utils.rnn.pad_sequence(inputs, batch_first=True, padding_value=0)
+# shape
+print(inputs.size())
+# 값
+print(inputs)
+
+n_vocab = len(vocab) # vocab count
+
+d_hidn = 128 # hidden size
+nn_emb = nn.Embedding(n_vocab, d_hidn) # embedding 객체
+
+input_embs = nn_emb(inputs) # input embedding
+print(input_embs.size())
+
+def get_sinusoid_encoding_table(n_seq, d_hidn):
+    def cal_angle(position, i_hidn):
+        return position / np.power(10000, 2 * (i_hidn // 2) / d_hidn)
+    def get_posi_angle_vec(position):
+        return [cal_angle(position, i_hidn) for i_hidn in range(d_hidn)]
+
+    sinusoid_table = np.array([get_posi_angle_vec(i_seq) for i_seq in range(n_seq)])
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # even index sin
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # odd index cos
+
+    return sinusoid_table
+
+n_seq = 64
+pos_encoding = get_sinusoid_encoding_table(n_seq, d_hidn)
+
+print (pos_encoding.shape) # 크기 출력
+
+pos_encoding = torch.FloatTensor(pos_encoding)
+nn_pos = nn.Embedding.from_pretrained(pos_encoding, freeze=True)
+
+positions = torch.arange(inputs.size(1), device=inputs.device, dtype=inputs.dtype).expand(inputs.size(0), inputs.size(1)).contiguous() + 1
+pos_mask = inputs.eq(0)
+
+positions.masked_fill_(pos_mask, 0)
+pos_embs = nn_pos(positions) # position embedding
+
+print(inputs)
+print(positions)
+print(pos_embs.size())
