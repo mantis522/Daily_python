5581873262cb5bb414c53267aa3f4f18da5eeed7
mantis522 <augustnorase@gmail.com>
2021-04-15 00:19:40 +0900

    reference update

--- deep_learning/Summarization/reference.py
+++ deep_learning/Summarization/reference.py
@@ -1,2 +1,4 @@
 ## https://www.kaggle.com/rahuldshetty/text-summarization-in-pytorch
 ## https://www.kaggle.com/sumantindurkhya/text-summarization-seq2seq-pytorch
+
+## pointer generator : https://github.com/atulkum/pointer_summarizer
